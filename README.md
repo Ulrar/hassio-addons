d52f5ccf3400ace2396cd992c752948e9ac00a99
Kevin Lemonnier <kevin.lemonnier@cognix-systems.com>
2020-06-05 13:30:44 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# hassio-addons
-Repo for hassio addons
+# Forked-DAAPD
+
+Based on [johnpdowling's addon](https://github.com/johnpdowling/hassio-addons/tree/master/forked-daapd), with spotify, chromecast and airplay added.
+The configuration tab isn't implemented yet, you will need to edit the config file in /share/forked-daapd/forked-daapd.conf if you want to change anything.
+The defaults should work fine for most uses.
---
+++ forked-daapd/rootfs/etc/cont-init.d/forked-daapd.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+# JPD Hass.io Add-ons: forked-daapd server
+# Configures the forked-daapd Server
+# ==============================================================================
+
+if ! bashio::fs.directory_exists '/share/forked-daapd/cache'; then
+    bashio::log.debug 'Creating cache folder...'
+    mkdir -p /share/forked-daapd/cache
+fi
+
+if ! bashio::fs.file_exists '/share/forked-daapd/forked-daapd.conf'; then
+    bashio::log.debug 'Copying default conf file...'
+    cp /usr/local/etc/forked-daapd.conf /share/forked-daapd/forked-daapd.conf
+fi
+
+if ! bashio::fs.directory_exists '/share/forked-daapd/music'; then
+    bashio::log.debug 'Creating music folder...'
+    mkdir -p /share/forked-daapd/music
+    bashio::log.debug 'Creating HA fifo file...'
+    mkfifo -m 666 /share/forked-daapd/music/HomeAssistantAnnounce
+fi
